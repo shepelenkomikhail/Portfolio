655d47059ec43e610ce2d8c1335eac469c8cd71b
Mykhailo Shepelenko <mikhael2005@gmail.com>
2024-04-20 19:13:37 +0200

    Added projects and skills

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,4 +1,45 @@
 import './public/styles.css'
-
-//document.body.classList.add('bg-black');
 AOS.init();
+
+const images = {
+    "Java logo": `<img src="/java.png" alt="Java logo">`,
+    "C# logo": `<img src="/CS.png" alt="C# logo">`,
+    "Python logo": `<img src="/python.png" alt="Python logo">`,
+    "C++ logo": `<img src="/cpp.png" alt="C++ logo">`,
+    "SQL logo": `<img src="/sql.png" alt="SQL logo">`,
+    "JS logo": `<img src="/js.png" alt="JS logo" class="rounded-full">`,
+    "PHP logo": `<img src="/php.png" alt="PHP logo">`,
+    "HTML logo": `<img src="/html.png" alt="HTML logo" >`,
+    "CSS logo": `<img src="/css.png" alt="CSS logo">`,
+    "Tailwindcss logo": `<img src="/tailw.png" alt="Tailwindcss logo">`,
+    "Figma logo": `<img src="/figma.png" alt="Figma logo">`,
+    "Git hub logo": `<img src="/gh.png" alt="Git hub logo">`,
+    "Git lab logo": `<img src="/gl.png" alt="Git lab logo">`,
+    "Vite logo": `<img src="/vite.png" alt="Vite logo">`
+  };
+
+  const skillsTable = document.querySelector('#skills');
+  let count = 0;
+  let row = createRow();
+  
+  for (const key in images) {
+      if (count % 5 === 0 && count !== 0) {
+          skillsTable.appendChild(row);
+          row = createRow();
+      }
+      const td = document.createElement('td');
+      const image = images[key];
+      td.innerHTML = image;
+      td.style.padding = '30px';
+      row.appendChild(td);
+      count++;
+  }
+
+  if (row.children.length > 0) {
+      skillsTable.appendChild(row);
+  }
+  
+  function createRow() {
+      return document.createElement('tr');
+  }
+
