5e51a9b3f1763c0cad7bcf8ff834a89c9c828882
Mykhailo Shepelenko <mikhael2005@gmail.com>
2024-11-30 18:05:52 +0100

    minor refactor

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,13 @@ import Projects from "./pages/Projects.tsx";
 function App() {
 
   return (
-    <>
-        <Router basename={"/"}>
-           <Routes>
-               <Route path="/" element={<Main />} />
-               <Route path="/aboutme" element={<About />} />
-               <Route path="/projects" element={<Projects />} />
-          </Routes>
-        </Router>
-    </>
+      <Router basename={"/"}>
+          <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/aboutme" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
   )
 }
 
